1079833812d836034ecf4f3703be59a6e0beea2d
Bitok85 <kan041185@gmail.com>
2021-07-26 17:20:27 +0300

    3.1. Массив с пустыми ячейками [#176470]

---
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -0,0 +1,19 @@
+package ru.job4j.pojo;
+
+public class Shop extends Product{
+
+    public Shop(String name, int count) {
+        super(name, count);
+    }
+
+    public static int indexOfNull(Product[] products) {
+        int rsl = -1;
+        for (int i = 0; i < products.length; i++) {
+            if (products[i] == null) {
+                rsl = i;
+                break;
+            }
+        }
+        return rsl;
+    }
+}
