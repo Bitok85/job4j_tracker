d8499a5f764712a374b65fbbeb0d9f7a8c936b86
Bitok85 <kan041185@gmail.com>
2021-07-26 09:04:08 +0300

    6. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,12 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created;
 
     public Item(){
     }
@@ -13,6 +17,10 @@ public class Item {
     public Item(int id, String name){
         this.id = id;
         this.name = name;
+    }
+
+    public LocalDateTime getDateTime(){
+        return created.now();
     }
 
     public int getId() {
---
+++ src/main/java/ru/job4j/tracker/profession/StartUI.java
@@ -0,0 +1,17 @@
+package ru.job4j.tracker.profession;
+
+import ru.job4j.tracker.Item;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI extends Item {
+
+    public static void main(String[] args) {
+        Item testDateTime = new Item();
+        LocalDateTime dirtyDateTime = testDateTime.getDateTime();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String cleanDateTime = dirtyDateTime.format(formatter);
+        System.out.println(cleanDateTime);
+    }
+}
