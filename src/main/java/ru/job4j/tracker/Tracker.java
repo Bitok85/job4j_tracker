a190d5a7cfed85cd8d226fd771cf07a417164c75
Bitok85 <kan041185@gmail.com>
2021-07-27 20:27:46 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -22,4 +22,37 @@ public class Tracker {
         }
         return rsl;
     }
+
+    public Item[] findAll() {
+        int count = 0;
+        for (Item item : items) {
+            if (item == null) {
+                break;
+            }
+            count++;
+        }
+        Item[] itemsWoNull = new Item[count];
+        for (int i = 0; i < itemsWoNull.length; i++) {
+            itemsWoNull[i] = items[i];
+        }
+        return itemsWoNull;
+    }
+
+    public Item[] findByName(String key) {
+        int count = 0;
+        for (Item item: items) {
+            if (item != null && key.equals(item.getName())) {
+                count++;
+            }
+        }
+        Item[] nameArray = new Item[count];
+        count = 0;
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null && key.equals(items[i].getName())) {
+                nameArray[count] = items[i];
+                count++;
+            }
+        }
+        return nameArray;
+    }
 }
