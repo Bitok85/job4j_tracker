0fa393690d949f75d1a97a8b9c5552c510ea2831
Bitok85 <kan041185@gmail.com>
2021-07-28 18:16:04 +0300

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,7 +5,7 @@ import ru.job4j.tracker.Item;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class StartUI extends Item {
+public class StartUI {
 
     public static void main(String[] args) {
         Item testDateTime = new Item();
@@ -13,5 +13,7 @@ public class StartUI extends Item {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
         String cleanDateTime = dirtyDateTime.format(formatter);
         System.out.println(cleanDateTime);
+        Item testToStr = new Item(12, "Gav");
+        System.out.println(testToStr.toString());
     }
 }
