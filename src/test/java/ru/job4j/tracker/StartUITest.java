ebd5a6ccca389c8a2fe80ac33e23eade91373e23
Bitok85 <kan041185@gmail.com>
2021-08-03 16:15:05 +0300

    9. Написать тесты на StartUI. [#785]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -3,6 +3,7 @@ package ru.job4j.tracker;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.Matchers.nullValue;
 
 public class StartUITest {
 
@@ -45,6 +46,51 @@ public class StartUITest {
         deleteAction.execute(new StubInput(answers), tracker);
         Item deleted = tracker.findById(item.getId());
         assertNull(deleted);
+    }
+
+    @Test
+    public void whenCreateItemUI() {
+        Input in = new StubInput(
+                new String[] {"0", "test", "1"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new CreateAction(),
+                new ExitAction()
+        };
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findAll()[0].getName(), is ("test"));
+    }
+
+    @Test
+    public void whenReplaceItemUI() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test"));
+        String replacedName = "test2";
+        Input in = new StubInput(
+                new String[] {"0", "1", replacedName}
+        );
+        UserAction[] actions = {
+                new ReplaceAction(),
+                new ExitAction()
+        };
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findById(item.getId()).getName(), is (replacedName));
+    }
+
+    @Test
+    public void whenDeleteItemUI() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("Deleted item"));
+        Input in = new StubInput(
+                new String[] {"0", "1"}
+        );
+        UserAction[] actions = {
+                new DeleteAction(),
+                new ExitAction()
+        };
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findById(item.getId()), is (nullValue()));
     }
 }
 
