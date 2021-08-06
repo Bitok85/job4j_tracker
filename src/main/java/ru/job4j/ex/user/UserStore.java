5a286a56c0b897ad701fa3ae66071ed15aaf1194
Bitok85 <kan041185@gmail.com>
2021-08-06 20:41:17 +0300

    0.5. Иерархия исключений и множественный catch. [#219368]

--- src/main/java/ru/job4j/ex/user/UserStore.java
+++ src/main/java/ru/job4j/ex/user/UserStore.java
@@ -16,14 +16,10 @@ public class UserStore {
     }
 
     public static boolean validate (User user) throws  UserInvalidException {
-        boolean rsl = false;
-        if (user.isValid() && user.getUsername().length() >= 3) {
-            rsl = true;
-        }
-        if (!rsl) {
+        if (!user.isValid() || user.getUsername().length() < 3) {
             throw  new UserInvalidException("User isn't valid");
         }
-        return rsl;
+        return true;
     }
 
     public static void main(String[] args) {
