93cbb180060ad99d3488cfe0eabc3c3412e96256
Bitok85 <kan041185@gmail.com>
2021-09-23 13:23:01 +0300

    2.2. Лямбда блок [#249208]

---
+++ src/main/java/ru/job4j/function/LambdaUsage.java
@@ -0,0 +1,12 @@
+package ru.job4j.function;
+
+import java.util.Comparator;
+
+public class LambdaUsage {
+    public static void main(String[] args) {
+        Comparator<String> comp = (left, right) -> {
+            System.out.println("Compare - " + right.length() + " : " + left.length());
+            return right.length() - left.length();
+        };
+    }
+}
