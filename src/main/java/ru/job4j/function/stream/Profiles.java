9803900b8d481d5e767a0a684dd3bb5b9f6ab57c
Bitok85 <kan041185@gmail.com>
2021-09-25 22:32:00 +0300

    3. Уникальность элементов и сортировка. [#110225]

--- src/main/java/ru/job4j/function/stream/Profiles.java
+++ src/main/java/ru/job4j/function/stream/Profiles.java
@@ -1,12 +1,20 @@
 package ru.job4j.function.stream;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(
+        List<Address> rsl = new ArrayList<>();
+        Comparator<Address> comp = (left, right) -> left.getCity().compareTo(right.getCity());
+        rsl = profiles.stream().map(
                 Profile::getAddress
         ).collect(Collectors.toList());
+        rsl.sort(comp);
+        return rsl.stream().
+                distinct()
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/ru/job4j/function/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/function/stream/ProfilesTest.java
@@ -26,4 +26,40 @@ public class ProfilesTest {
         assertThat(rsl.get(0).getCity(), is(expected1));
         assertThat(rsl.get(1).getHome(), is(expected2));
     }
+
+    @Test
+    public void whenSameAddress() {
+        Profiles pr = new Profiles();
+        Address address1 = new Address("Tver", "Lenina str", 5, 10);
+        Address address2 = new Address("Moscow", "Stroiteley str", 5, 2);
+        Address address3 = new Address("Tver", "Lenina str", 5, 10);
+        Profile profile1 = new Profile(address1);
+        Profile profile2 = new Profile(address2);
+        Profile profile3 = new Profile(address3);
+        List<Profile> profiles = List.of(
+                profile1,
+                profile2,
+                profile3
+        );
+        List<Address> rsl = pr.collect(profiles);
+        assertThat(rsl.size(), is(2));
+    }
+
+    @Test
+    public void whenSameCityAndNotSameAddress() {
+        Profiles pr = new Profiles();
+        Address address1 = new Address("Tver", "Lenina str", 5, 10);
+        Address address2 = new Address("Moscow", "Stroiteley str", 5, 2);
+        Address address3 = new Address("Tver", "Stroiteley str", 2, 25);
+        Profile profile1 = new Profile(address1);
+        Profile profile2 = new Profile(address2);
+        Profile profile3 = new Profile(address3);
+        List<Profile> profiles = List.of(
+                profile1,
+                profile2,
+                profile3
+        );
+        List<Address> rsl = pr.collect(profiles);
+        assertThat(rsl.get(1).getCity(), is(rsl.get(2).getCity()));
+    }
 }
