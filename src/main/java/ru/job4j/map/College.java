885d3a9353aa24b5ac26a8b2eaf8364e43aa4224
Bitok85 <kan041185@gmail.com>
2021-09-28 11:20:50 +0300

    College

---
+++ src/main/java/ru/job4j/map/College.java
@@ -0,0 +1,32 @@
+package ru.job4j.map;
+
+import java.util.Map;
+import java.util.Set;
+
+public class College {
+    private final Map<Student, Set<Subject>> students;
+
+    public College(Map<Student, Set<Subject>> students) {
+        this.students = students;
+    }
+
+    public Student findByAccount(String name) {
+        return students.keySet()
+                .stream()
+                .filter(student -> student.getName().equals(name))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public Subject findBySubjectName(String account, String name) {
+        Student student = findByAccount(account);
+        if (student != null) {
+            return students.get(student)
+                    .stream()
+                    .filter(s -> s.getName().equals(name))
+                    .findFirst()
+                    .orElse(null);
+        }
+        return null;
+    }
+}
