3ce8d4a0b0408a3ee40406275d38df3cc0ae99b6
Bitok85 <kan041185@gmail.com>
2021-09-29 10:48:43 +0300

    Чтобы не висело

--- src/main/java/ru/job4j/function/stream/Student.java
+++ src/main/java/ru/job4j/function/stream/Student.java
@@ -28,11 +28,12 @@ public class Student {
             return false;
         }
         Student student = (Student) o;
-        return score == student.score && Objects.equals(surname, student.surname);
+        return score == student.score
+                && Objects.equals(surname, student.surname);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(score, surname);
+        return Objects.hash(surname, score);
     }
 }
