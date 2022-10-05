abb6062ab92e0575f32c921103f40757828dcc0e
Bitok85 <kan041185@gmail.com>
2022-10-05 14:19:58 +0300

    1. Конфигурация Hibernate [#6872]

---
+++ src/main/java/ru/job4j/tracker/HQLUsage.java
@@ -0,0 +1,29 @@
+package ru.job4j.tracker;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.query.Query;
+
+public class HQLUsage {
+
+    public static void main(String[] args) {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+        try (SessionFactory sf = new MetadataSources(registry)
+                .buildMetadata().buildSessionFactory()) {
+            Session session = sf.openSession();
+            session.beginTransaction();
+            Query query = session.createQuery("from Item");
+            query.list().forEach(System.out::println);
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+}
